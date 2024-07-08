9f74d704ce6647ae8c1814d1927c03f13b68f9e6
Keitu_Lerato <120262068+Kei2Masilo@users.noreply.github.com>
2024-07-08 12:00:42 +0200

    Add files via upload

---
+++ salon.js
@@ -0,0 +1,10 @@
+// Get values from form
+var username = document.getElementById("username").value;
+var phoneNumber = document.getElementById("phoneNumber").value;
+var appointmentTime = document.getElementById("appointmentTime").value;
+var services = document.getElementById("checkbox").value;
+var deposit = document.getElementById("deposit").value;
+
+var name = document.getElementById("displayName").innerText = "Dear , " + username;
+
+console.log(username);
